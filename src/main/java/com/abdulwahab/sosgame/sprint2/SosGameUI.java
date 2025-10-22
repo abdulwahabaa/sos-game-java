123a71b98e5f3014f785d377c1b98366067b1652
Abdulwahab <abdulwahababdi94@gmail.com>
2025-10-21 20:56:36 -0500

    Added basic window setup

--- src/main/java/com/abdulwahab/sosgame/sprint2/SosGameUI.java
+++ src/main/java/com/abdulwahab/sosgame/sprint2/SosGameUI.java
@@ -1,4 +1,16 @@
 package com.abdulwahab.sosgame.sprint2;
 
-public class SosGameUI {
+import javax.swing.*;
+import java.awt.*;
+
+public class SosGameUI extends JFrame {
+
+    public SosGameUI() {
+        setTitle("SosGame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
