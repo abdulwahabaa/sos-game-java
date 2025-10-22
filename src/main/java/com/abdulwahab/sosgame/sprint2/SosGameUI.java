1c38f70504f50c9f34294944021776ea6ee69e72
Abdulwahab <abdulwahababdi94@gmail.com>
2025-10-21 21:09:11 -0500

    Added top panel with game mode and board size

--- src/main/java/com/abdulwahab/sosgame/sprint2/SosGameUI.java
+++ src/main/java/com/abdulwahab/sosgame/sprint2/SosGameUI.java
@@ -9,6 +9,25 @@ public class SosGameUI extends JFrame {
         setTitle("SosGame");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+        //Window setup
+
+        JPanel topPanel = new JPanel();
+        JRadioButton simpleGame = new JRadioButton("Simple game", true);
+        JRadioButton generalGame = new JRadioButton("General game");
+        ButtonGroup modeGroup = new ButtonGroup();
+        modeGroup.add(simpleGame);
+        modeGroup.add(generalGame);
+
+        JTextField boardSizeField = new JTextField("5", 3);
+
+        topPanel.add(new JLabel("SOS"));
+        topPanel.add(simpleGame);
+        topPanel.add(generalGame);
+        topPanel.add(new JLabel("Board size:"));
+        topPanel.add(boardSizeField);
+        add(topPanel, BorderLayout.NORTH);
+        //Game mode and board size
+
 
         pack();
         setLocationRelativeTo(null);
